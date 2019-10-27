ecaf4b2f47934a5ed21a68c25b0493adee2c7511
naknoPC <osq1993@protonmail.com>
2019-10-27 17:15:01 +0100

    Created a name generator.

---
+++ script.js
@@ -0,0 +1,19 @@
+let forename_prefix = ['Mo', 'He', 'Ja', 'Schmu', 'Schlo', 'Meno', 'Eli', 'Shi', 'Yeru', 'Reu'];
+let forename_suffix = ['donai', 'shel', 'mon', 'mo', 'diah', 'chay', 'prahim', 'muel', 'liel', 'bediah'];
+let surname_prefix = ['Gold', 'Wasser', 'Edel', 'Bergen', 'Braun', 'Katzen', 'Baren', 'Apple', 'Blumen', 'Kronen'];
+let surname_suffix = ['berg', 'stein', 'meyer', 'owsky', 'heimer', 'baum', 'feld', 'bach', 'heimer', 'mann'];
+
+function rndint(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generate_jewish_name() {
+  jewish_name.textContent = forename_prefix[rndint(0,9)] + forename_suffix[rndint(0,9)] + ' ' + surname_prefix[rndint(0,9)] + surname_suffix[rndint(0,9)];
+}
+
+const jewish_name = document.querySelector('.jewish_name');
+const jew_button = document.querySelector('.jew_button');
+
+jew_button.addEventListener('click', generate_jewish_name);
